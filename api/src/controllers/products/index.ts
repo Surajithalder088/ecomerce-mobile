4875354c059b40191a5578965769b87174130d6c
Surajit <surajithalder088@gmail.com>
2025-01-19 23:58:41 +0530

    products rout set up

--- api/src/controllers/products/index.ts
+++ api/src/controllers/products/index.ts
@@ -1,5 +1,18 @@
 import { Request ,Response} from "express"
 
 export const getLists=(req :Request,res:Response)=>{
-        res.send("working")
+        res.send("list of products")
+}
+
+export const getProductById=(req :Request,res:Response)=>{
+    res.send("a product")
+}
+export const createProduct=(req :Request,res:Response)=>{
+    res.send("a product created")
+}
+export const updateProduct=(req :Request,res:Response)=>{
+    res.send("a product updated")
+}
+export const deleteProduct=(req :Request,res:Response)=>{
+    res.send("a product deleted")
 }
