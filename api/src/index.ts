6eaaad9769fbe3161eb5cf27e84686fd18750802
Surajit <surajithalder088@gmail.com>
2025-01-19 23:38:17 +0530

    product router

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express"
-
+import productRoutes from "./routes/products/index"
 
 const app=express();
 
 const port=process.env.PORT || 4000
+
+app.use("/api/products",productRoutes)
+
+
 
 app.listen(port,()=>{
     console.log("server  running on :",port);
